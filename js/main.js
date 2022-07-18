eb3b12a0bee955941b970e5013a816fc5fa3e8b6
sourceweb53 <sourceweb53@gmail.com>
2022-07-18 22:05:28 +0300

    адаптировал меню

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Обработчик для burger меню
+
+burgerMenu.addEventListener('click', function () {
+    burgerMenu.classList.toggle('active-menu-burger');
+    wrapperBurgerMenu.classList.toggle('active-wrapper-menu');
+});
+
