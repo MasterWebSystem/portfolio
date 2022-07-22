6af9eb446ff3ebf6480619544e5be02301b7c861
sourceweb53 <sourceweb53@gmail.com>
2022-07-22 21:14:06 +0300

    Верстка третьего экрана

--- js/main.js
+++ js/main.js
@@ -25,8 +25,11 @@ linkHello.addEventListener('click', () => {
   linkHello.classList.add('active-list-link');
   linkAboutItem.classList.remove('active-list-item');
   linkAbout.classList.remove('active-list-link');
+  linkProjectsItem.classList.remove('active-list-item');
+  linkProjects.classList.remove('active-list-link');
   mainBlock.style.display = 'flex'
   aboutBlock.style.display = 'none';
+  projectsBlock.style.display = 'none';
 
 });
 
@@ -35,8 +38,23 @@ linkAbout.addEventListener('click', () => {
     linkAbout.classList.add('active-list-link');
     linkHelloItem.classList.remove('active-list-item');
     linkHello.classList.remove('active-list-link');
+    linkProjectsItem.classList.remove('active-list-item');
+    linkProjects.classList.remove('active-list-link');
     aboutBlock.style.display = 'flex';
     mainBlock.style.display = 'none';
+    projectsBlock.style.display = 'none';
+});
+
+linkProjects.addEventListener('click', () => {
+    linkProjectsItem.classList.add('active-list-item');
+    linkProjects.classList.add('active-list-link');
+    linkHelloItem.classList.remove('active-list-item');
+    linkHello.classList.remove('active-list-link');
+    linkAboutItem.classList.remove('active-list-item');
+    linkAbout.classList.remove('active-list-link');
+    aboutBlock.style.display = 'none';
+    mainBlock.style.display = 'none';
+    projectsBlock.style.display = 'flex';
 });
 
 // Переключатель burgerMenu 
